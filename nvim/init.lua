82ca0dd93fb606a531d5cef9314a2eeee0b68483
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2022-11-07 20:07:02 +0300

    Add colorscheme and completion plugins
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,6 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -40,9 +40,20 @@ packer.init {
 
 return packer.startup(function(use)
   -- My plugins here
-  use "wbthomason/packer.nvim" 		-- Have packer manage itself
-  use "nvim-lua/popup.nvim" 		-- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" 		-- Useful lua functions used ny lots of plugins
+  use "wbthomason/packer.nvim" 			-- Have packer manage itself
+  use "nvim-lua/popup.nvim" 			-- An implementation of the Popup API from vim in Neovim
+  use "nvim-lua/plenary.nvim" 			-- Useful lua functions used ny lots of plugins
+  use "folke/tokyonight.nvim"			-- colorscheme
+  
+  use "hrsh7th/nvim-cmp" 				-- The completion plugin
+  use "hrsh7th/cmp-buffer" 				-- buffer completions
+  use "hrsh7th/cmp-path" 				-- path completions
+  use "saadparwaiz1/cmp_luasnip" 		-- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+
+  use "L3MON4D3/LuaSnip" 				--snippet engine
+  use "rafamadriz/friendly-snippets" 	-- a bunch of snippets to use
 
   -- Automatically set up your configuration after cloning packer.nvim
   if PACKER_BOOTSTRAP then
