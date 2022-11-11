6a43af234aa24b015fd4c989da62d8e5982e02c9
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2022-11-11 11:13:30 +0300

    Add nvim illuminate
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -84,6 +84,7 @@ return packer.startup(function(use)
 	use "moll/vim-bbye"
 	use "lewis6991/impatient.nvim"
 	use "lukas-reineke/indent-blankline.nvim"
+	use "RRethy/vim-illuminate"
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	if PACKER_BOOTSTRAP then
