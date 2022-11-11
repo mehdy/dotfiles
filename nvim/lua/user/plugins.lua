ad97924adfa9ad608e4274b16efa9925e3af5747
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2022-11-11 11:11:49 +0300

    Add nvim indentline
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -83,6 +83,7 @@ return packer.startup(function(use)
 	use "nvim-lualine/lualine.nvim"
 	use "moll/vim-bbye"
 	use "lewis6991/impatient.nvim"
+	use "lukas-reineke/indent-blankline.nvim"
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	if PACKER_BOOTSTRAP then
