78bca37495084c3dffc3838a6674addb242e7efe
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2022-11-13 19:26:02 +0300

    Update install.sh
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
+ln -sf $PWD/{.alacritty.yml,.vimrc,.zshrc,.tmux.conf} ~/
+ln -sf $PWD/starship.toml ~/.config/
+ln -sf $PWD/nvim ~/.config/
+
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 	~/.tmux/plugins/tpm/bin/install_plugins
 fi
-
-ln -sf $PWD/{.alacritty.yml,.vimrc,.zshrc,.tmux.conf} ~/
-ln -sf $PWD/starship.toml ~/.config/
-ln -sf $PWD/nvim ~/.config/
