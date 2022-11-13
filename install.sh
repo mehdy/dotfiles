f2f2dacdc4ebd12fdcb258a8c2850438c2d677a8
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2022-11-13 13:22:51 +0300

    Add alacritty config
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-ln -sf $PWD/{.vimrc,.zshrc,.tmux/.tmux.conf,.tmux.conf.local} ~/
+ln -sf $PWD/{.alacritty.yml,.vimrc,.zshrc,.tmux/.tmux.conf,.tmux.conf.local} ~/
 ln -sf $PWD/starship.toml ~/.config/
 ln -sf $PWD/nvim ~/.config/
