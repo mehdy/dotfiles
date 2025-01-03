63446ea7ecdfdc15cbb4117ad9928cc25d4e0f7c
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2025-01-03 12:41:24 +0100

    Update hypr
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -sf $PWD/{.alacritty.toml,.vimrc,.zshrc,.tmux.conf,.gitconfig} ~/
 ln -sf $PWD/starship.toml ~/.config/
 ln -sf $PWD/nvim ~/.config/
+ln -sf $PWD/hypr/ ~/.config/
 
 if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
