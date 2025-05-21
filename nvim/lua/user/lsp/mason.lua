5c7d5b962889e6a45c83cbbfca728e64bda37acc
Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>
2025-05-21 09:26:27 +0200

    Disable automatic_enable of LSPs
    Signed-off-by: Mehdi Khoshnoodi <mehdy.khoshnoody@gmail.com>

--- nvim/lua/user/lsp/mason.lua
+++ nvim/lua/user/lsp/mason.lua
@@ -32,6 +32,7 @@ require("mason").setup(settings)
 require("mason-lspconfig").setup({
 	ensure_installed = servers,
 	automatic_installation = true,
+	automatic_enable = false,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
